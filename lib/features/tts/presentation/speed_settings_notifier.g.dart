840b98c25231d97948a3b1f83050a3499a8568fe
aoisakanana <bluedust0152@gmail.com>
2025-12-28 01:50:00 +0900

    build-runner実行

---
+++ lib/features/tts/presentation/speed_settings_notifier.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'speed_settings_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$speedSettingsNotifierHash() =>
+    r'95515f04431dd79206f7263c220ae0fcb9b51f4d';
+
+/// 読み上げ速度設定の状態を管理する Notifier
+///
+/// TtsService と連携して速度設定を管理します。
+///
+/// Copied from [SpeedSettingsNotifier].
+@ProviderFor(SpeedSettingsNotifier)
+final speedSettingsNotifierProvider =
+    AutoDisposeNotifierProvider<SpeedSettingsNotifier, double>.internal(
+  SpeedSettingsNotifier.new,
+  name: r'speedSettingsNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$speedSettingsNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SpeedSettingsNotifier = AutoDisposeNotifier<double>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
